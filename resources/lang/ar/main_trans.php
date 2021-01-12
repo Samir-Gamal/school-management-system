69b8f48a4ccaa43b3190e4c36be3b1fb92e15886
Samir Gamal <samir.gamal77@yahoo.com>
2021-01-12 22:37:19 +0300

    update_code

---
+++ resources/lang/ar/main_trans.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'Dashboard'=>'لوحة التحكم',
+    'Dashboard_page'=>'لوحة التحكم',
+    'change_language'=>'تغير اللغة'
+
+
+
+];
---
+++ resources/lang/en/main_trans.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+'Dashboard'=>'Dashboard',
+    'Dashboard_page'=>'Dashboard',
+    'change_language'=>'language'
+
+];
