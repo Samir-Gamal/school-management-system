875cc0ad0d03d224da95ca2d512abceadec0180e
Samir Gamal <69224131+Samir-Gamal@users.noreply.github.com>
2021-02-10 23:33:20 +0300

    sections

---
+++ database/migrations/2021_02_11_181424_create_foreign_keys.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Eloquent\Model;
+
+class CreateForeignKeys extends Migration {
+
+	public function up()
+	{
+		Schema::table('Classrooms', function(Blueprint $table) {
+			$table->foreign('Grade_id')->references('id')->on('Grades')
+						->onDelete('cascade');
+		});
+
+        Schema::table('sections', function(Blueprint $table) {
+            $table->foreign('Grade_id')->references('id')->on('Grades')
+                ->onDelete('cascade');
+        });
+
+            Schema::table('sections', function(Blueprint $table) {
+                $table->foreign('Class_id')->references('id')->on('Classrooms')
+                    ->onDelete('cascade');
+            });
+
+	}
+
+	public function down()
+	{
+		Schema::table('Classrooms', function(Blueprint $table) {
+			$table->dropForeign('Classrooms_Grade_id_foreign');
+		});
+        Schema::table('sections', function(Blueprint $table) {
+            $table->dropForeign('sections_Grade_id_foreign');
+        });
+        Schema::table('sections', function(Blueprint $table) {
+            $table->dropForeign('sections_Class_id_foreign');
+        });
+	}
+}
