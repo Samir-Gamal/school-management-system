32ae31837fbfc2e34fdd8c70c6c3909780974a0f
fh32000 <fh32000@gmail.com>
2021-08-10 23:12:02 +0300

    modify logos and favicon and lang flag images

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -33,7 +33,7 @@ header start-->
             <button type="button" class="btn btn-light btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               @if (App::getLocale() == 'ar')
               {{ LaravelLocalization::getCurrentLocaleName() }}
-             <img src="{{ URL::asset('assets/images/flags/EG.png') }}" alt="">
+             <img src="{{ URL::asset('assets/images/flags/SA.png') }}" alt="">
               @else
               {{ LaravelLocalization::getCurrentLocaleName() }}
               <img src="{{ URL::asset('assets/images/flags/US.png') }}" alt="">
