40aae70d2889ba6c99045046933d0bc6b33f9c4f
fh32000 <fh32000@gmail.com>
2021-08-16 10:47:14 +0300

    install Laravel IDE Helper Generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
