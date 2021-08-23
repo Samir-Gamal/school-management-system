356136f3ca295d10f26f3e008da48e0812a49212
fh32000 <fh32000@gmail.com>
2021-08-23 14:22:03 +0300

    storage  files   to sub folder in s3

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,6 +70,7 @@ return [
 
         's3' => [
             'driver' => 's3',
+            'root' => 'sms/',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
