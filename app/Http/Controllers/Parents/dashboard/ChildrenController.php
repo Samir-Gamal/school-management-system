55d82a82b863e1590b133628d57abef5fded5144
Samir Gamal <samir.gamal77@yahoo.com>
2022-08-16 19:33:07 +0300

    get_children_in_dashboard_parent

---
+++ app/Http/Controllers/Parents/dashboard/ChildrenController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Parents\dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Degree;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class ChildrenController extends Controller
+{
+    public function index()
+    {
+
+        $students = Student::where('parent_id', auth()->user()->id)->get();
+        return view('pages.parents.children.index', compact('students'));
+    }
+
+    public function results($id)
+    {
+
+        $student = Student::findorFail($id);
+
+        if ($student->parent_id !== auth()->user()->id) {
+            toastr()->error('يوجد خطا في كود الطالب');
+            return redirect()->route('sons.index');
+        }
+        $degrees = Degree::where('student_id', $id)->get();
+
+        if ($degrees->isEmpty()) {
+            toastr()->error('لا توجد نتائج لهذا الطالب');
+            return redirect()->route('sons.index');
+        }
+
+        return view('pages.parents.degrees.index', compact('degrees'));
+
+    }
+}
