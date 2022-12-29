7346988d9cb04d01516cd030f7330810abb7059c
Yassir <yassirissoughaine@gmail.com>
2022-12-29 18:58:34 +0100

    files lang configuration

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
+
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+        Route::get('/', function () {
+            return view('dashboard');
+        });
 });
